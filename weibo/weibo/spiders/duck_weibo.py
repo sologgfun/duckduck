249351805c337ed8ccb95f0d4e09620308319da5
wangjianfeng <wangjianfeng@cmss.chinamobile.com>
2020-08-26 09:37:32 +0800

    爬虫

--- weibo/weibo/spiders/duck_weibo.py
+++ weibo/weibo/spiders/duck_weibo.py
@@ -1,12 +1,9 @@
 import scrapy
 
 class Duck(scrapy.Spider):
-start_urlsz = [
-            'https://weibo.com/?category=0'
-        ]
-name = "weibo"
-start_urls = [
-            'https://weibo.com/?category=0'
+        name = "weibo"
+        start_urls = [
+                'https://weibo.com/?category=0'
         ]
 
 def parse(self, response):
